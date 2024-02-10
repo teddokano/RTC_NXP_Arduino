23dca2e84b2ad216752ca9fd053225d2275232f1
Tedd OKANO <okano.akifumi@gmail.com>
2024-02-11 08:38:34 +0900

    PCF85053A simple sample working

---
+++ examples/PCF85053A_simple/PCF85053A_simple.ino
@@ -0,0 +1,69 @@
+/** PCF85063A RTC operation sample
+ *  
+ *  This sample code is showing PCF85063A RTC operation.
+ *
+ *  @author  Tedd OKANO
+ *
+ *  Released under the MIT license License
+ *
+ *  About PCF85063A:
+ *    https://www.nxp.com/products/peripherals-and-logic/signal-chain/real-time-clocks/rtcs-with-ic-bus/tiny-real-time-clock-calendar-with-alarm-function-and-ic-bus:PCF85063A   
+ */
+
+#include <PCF85063A.h>
+#include <time.h>
+
+void set_time(void);
+
+PCF85063A rtc;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+
+  Wire.begin();
+
+  Serial.println("\n***** Hello, PCF85063A! *****");
+
+  if (rtc.oscillator_stop()) {
+    Serial.println("==== oscillator_stop detected :( ====");
+    set_time();
+  } else {
+    Serial.println("---- RTC has beeing kept running! :) ----");
+  }
+}
+
+void loop() {
+  time_t current_time = 0;
+
+  current_time = rtc.time(NULL);
+  Serial.print("time : ");
+  Serial.print(current_time);
+  Serial.print(", ");
+  Serial.println(ctime(&current_time));
+
+  delay(1000);
+}
+
+void set_time(void) {
+  /*  !!!! "strptime()" is not available in Arduino's "time.h" !!!!
+  const char* current_time  = "2023-4-7 05:25:30";
+  const char* format  = "%Y-%m-%d %H:%M:%S";
+  struct tm	tmv;
+  strptime( current_time, format, &tmv );
+  */
+
+  struct tm now_tm;
+
+  now_tm.tm_year = 2023 - 1900;
+  now_tm.tm_mon = 4 - 1;  // It needs to be '3' if April
+  now_tm.tm_mday = 7;
+  now_tm.tm_hour = 18;
+  now_tm.tm_min = 52;
+  now_tm.tm_sec = 30;
+
+  rtc.set(&now_tm);
+
+  Serial.println("RTC got time information");
+}
